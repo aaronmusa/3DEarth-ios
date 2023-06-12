fb8e931948598f6f6d02a63da00acfe2908148e7
Aaron Musa <jonasaaron.musa@appetiser.com.au>
2023-06-12 20:44:28 +0800

    Initial Commit

---
+++ 3DEarth/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  3DEarth
+//
+//  Created by Aaron Musa on 6/12/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
