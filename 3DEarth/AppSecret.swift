6a196e4521b4b6514d4ebc30c7620003fe8b963f
Aaron Musa <jonasaaron.musa@appetiser.com.au>
2023-06-15 02:15:09 +0800

    Final commit

--- 3DEarth/AppSecret.swift
+++ 3DEarth/AppSecret.swift
@@ -8,5 +8,6 @@
 import Foundation
 
 struct AppSecret {
+    /// Intentional: For demo purposes only
     static let defaultAccessToken = "pk.eyJ1IjoiYWFyb25tdXNhIiwiYSI6ImNsaXNscDB5NjB3ZGEzcHF2MW5zeThheGsifQ.S2CMBPohFBqzUa07HH289w"
 }
